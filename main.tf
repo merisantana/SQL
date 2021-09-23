d475944f7a653949a9a101fbbe2cd77a3c684b25
merisantana <86296843+merisantana@users.noreply.github.com>
2021-09-23 12:58:06 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+data "azurerm_resource_group" "RG" {
+  name = var.resurce_group_name
+}
+
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = var.namevnet
+  resource_group_name          = data.azurerm_resource_group.RG.name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "usuario"
+  administrator_login_password = "P4$$w0rdP4$$W0rd"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_sql_database" "sqldb" {
+  name                = var.namesqldb
+  resource_group_name = data.azurerm_resource_group.RG.name
+  location            = var.location
+  server_name         = azurerm_sql_server.sqlserver.name
+
+}
+
